79c67ee059382ccc2ee49ec98847f8167ad394f2
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-04-02 21:28:35 -0300

    react aula 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { HelloWorld } from "./HelloWorld"
+
+function App() {
+
+  return (
+    <>
+    
+    <h1>dantas</h1>
+    <h2>Campinas</h2>
+    <HelloWorld/>
+
+    
+    </>
+    
+  )
+}
+
+export default App
+
