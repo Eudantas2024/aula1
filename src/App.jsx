16cbf4c70cf60d8f857d9bea6ad418e98064c2ba
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-04-03 21:01:15 -0300

    component img

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Frase } from "./Frase"
 import { HelloWorld } from "./HelloWorld"
 import { Pessoa } from "./Pessoa"
 
+
+
 function App() {
 
   return (
@@ -10,11 +12,23 @@ function App() {
     <h1>dantas</h1>
     <Frase/>
     <h2>Campinas</h2>
-    <HelloWorld/>
-    <Pessoa />
-    
+    <HelloWorld />
+    <Pessoa
+       nome="Godinho"
+       idade="29"
+       profissao="Vigilante"
+       foto="https://th.bing.com/th?id=ORMS.b683165321a92c8ae5ad880ac7f77d9b&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1&p=0"
+    />
 
-    
+    <Pessoa
+       nome="Maria"
+       idade="38"
+       profissao="Médica"
+       foto="https://cdn.pixabay.com/photo/2025/03/28/11/46/butterfly-9498703_960_720.jpg"
+    />
+
+
+        
     </>
     
   )
--- src/Pessoa.jsx
+++ src/Pessoa.jsx
@@ -1,9 +1,29 @@
-export function Pessoa (){
-    return(
-        <>
-           <h2>Compras</h2>
-           <p>Clique para Comprar</p>
-        </>
-    )
 
+export function Pessoa(props) {
+  return (
+    <>
+    <div>
+        <img src={props.foto} alt={props.nome} />
+        <h2>Nome: {props.nome}</h2>
+        <p>Idade: {props.idade}</p>
+        <p>Profissão: {props.profissao}</p>
+    </div>
+    </>
+  )
 }
+
+// passagem de parametro, pro react é propriedade
+
+
+// export function Pessoa({foto, nome, idade, profissao}) {
+//     return (
+//       <>
+//       <div>
+//           <img src={foto} alt={nome} />
+//           <h2>Nome: {nome}</h2>
+//           <p>Idade: {idade}</p>
+//           <p>Profissão: {profissao}</p>
+//       </div>
+//       </>
+//     )
+//   }
